24c456806bbc37abafabbed8915c04dfcadbf6d8
Tobias Guse <tobias.guse@razerzone.com>
2017-06-06 13:10:09 +0200

    added database connection

--- server/index.js
+++ server/index.js
@@ -3,7 +3,24 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
 const app = express();
+
+// Connect to Database
+console.log('Connecting to database...');
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+  console.log(chalk.red('Could not connect to database!'));
+  console.error(chalk.red(err.message));
+});
+mongoose.connection.once('open', () => {
+  console.log(chalk.green('Successfully connected to database.'));
+});
+// Using helmet for some added security
+app.use(helmet());
 
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
@@ -13,6 +30,7 @@ app.get('/', function(req, res) {
   res.send('hello world! 123');
 });
 
-app.listen(8000, function() {
-  console.log('server is listening on port 8000');
+const port = process.env.PORT || 8000;
+app.listen(port, function() {
+  console.log(chalk.green(`server is listening on port ${port}.`));
 });
